c4f5348a14e242095832ed796a663d9f6e0cd326
noud <noud4@home.nl>
2024-04-16 18:40:03 +0200

    tests

---
+++ tests/Feature/DeviceControllerTest.php
@@ -0,0 +1,137 @@
+<?php
+
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+
+class DeviceControllerTest extends TestCase {
+    /**
+     * @var string
+     */
+    public $token = '';
+
+    public $employee_id;
+
+    public function setUp() :void
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        DB::table('devices')->where('name', 'device 1')->delete();
+        $response = $this->json('post', 'http://localhost/api/v1/register', [
+            'name' => 'noud3',
+            'email' => 'noud3@home.nl',
+            'password' => 'test1234',
+        ]);
+
+        $response = $this->json('post', 'http://localhost/api/v1/login', [
+            'email' => 'noud3@home.nl',
+            'password' => 'test1234',
+        ]);       
+        $response->assertStatus(200);
+        $token = $response['authorisation']['token'];
+        $this->token = $token;
+
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Noud de Brouwer',
+        ], ['Authorization' => 'Bearer ' . $this->token]);       
+        $response->assertStatus(200);
+        $employee_id = $response['employee']['id'];
+        $this->employee_id = $employee_id;
+
+    }
+    
+    public function tearDown() :void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Register test user
+     *
+     * @return void
+     */
+    public function testAddDevice()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/device', [
+            'name' => 'device 1',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => "Device created successfully",
+       ]);
+    }
+
+    public function testDeleteDevice()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/device', [
+            'name' => 'device 1',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $id = $response['device']['id'];
+
+        $response = $this->json('delete', 'http://localhost/api/v1/device/' . $id
+        , ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => "Device deleted successfully",
+       ]);
+    }
+
+    public function testShowDevice()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/device', [
+            'name' => 'device 1',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $id = $response['device']['id'];
+
+        $response = $this->json('get', 'http://localhost/api/v1/device/' . $id
+        , ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+       ]);
+    }
+
+    public function testUpdateDevice()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/device', [
+            'name' => 'device 1',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $id = $response['device']['id'];
+
+        $response = $this->json('put', 'http://localhost/api/v1/device/' . $id, [
+            'name' => 'device 2',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => "Device updated successfully",
+       ]);
+    }
+
+    public function testDevices()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/device', [
+            'name' => 'device 1',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $response = $this->json('post', 'http://localhost/api/v1/device', [
+            'name' => 'device 2',
+            'employee_id' => $this->employee_id,
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $response = $this->json('get', 'http://localhost/api/v1/devices?page=1',
+        ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+       ]);
+    }
+}
---
+++ tests/Feature/EmployeeControllerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+
+class EmployeeControllerTest extends TestCase {
+    /**
+     * @var string
+     */
+    public $token = '';
+
+    public function setUp() :void
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        DB::table('users')->where('name', 'noud5')->delete();
+        $response = $this->json('post', 'http://localhost/api/v1/register', [
+            'name' => 'noud3',
+            'email' => 'noud3@home.nl',
+            'password' => 'test1234',
+        ]);
+
+        $response = $this->json('post', 'http://localhost/api/v1/login', [
+            'email' => 'noud3@home.nl',
+            'password' => 'test1234',
+        ]);       
+        $response->assertStatus(200);
+        $token = $response['authorisation']['token'];
+        $this->token = $token;
+    }
+    
+    public function tearDown() :void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Register test user
+     *
+     * @return void
+     */
+    public function testAddEmployee()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Noud de Brouwer',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => "Employee created successfully",
+       ]);
+    }
+
+    public function testDeleteEmployee()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Noud de Brouwer',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $id = $response['employee']['id'];
+
+        $response = $this->json('delete', 'http://localhost/api/v1/employee/' . $id
+        , ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => "Employee deleted successfully",
+       ]);
+    }
+
+    public function testShowEmployee()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Noud de Brouwer',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $id = $response['employee']['id'];
+
+        $response = $this->json('get', 'http://localhost/api/v1/employee/' . $id
+        , ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+       ]);
+    }
+
+    public function testUpdateEmployee()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Noud de Brouwer',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $id = $response['employee']['id'];
+
+        $response = $this->json('put', 'http://localhost/api/v1/employee/' . $id, [
+            'name' => 'Mortada Abdul Roda',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+            'message' => "Employee updated successfully",
+       ]);
+    }
+
+    public function testEmployees()
+    {
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Noud de Brouwer',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $response = $this->json('post', 'http://localhost/api/v1/employee', [
+            'name' => 'Mortada Abdul Roda',
+        ], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $response = $this->json('get', 'http://localhost/api/v1/employees', [
+            'Authorization' => 'Bearer ' . $this->token]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'success',
+       ]);
+    }
+}
